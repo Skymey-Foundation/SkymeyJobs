f4a03a86224cbbbf2db21f5704263721888aafa8
dotnetcsharp0x <dsotnikov1996@gmail.com>
2024-11-10 13:52:06 +0300

    show crypto ticker data on the main page

---
+++ SkymeyIndexPageTokenList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SkymeyIndexPageTokenList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
